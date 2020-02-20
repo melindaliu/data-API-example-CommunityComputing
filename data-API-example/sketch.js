84e9477c995a430b81aca4e8eed5b95ab1e55d3f
melindaliu <meilnda.r.liu@gmail.com>
2020-02-20 15:35:55 -0800

    finished up to loading and coding longitude and latitude of ISS

--- data-API-example/sketch.js
+++ data-API-example/sketch.js
@@ -1,9 +1,7 @@
-
 let spaceData;
-
 function setup() {
   createCanvas(600, 600);
-  loadJSON('http://api.open-notify.org/astros.json', gotData, 'jsonp');
+  loadJSON('http://api.open-notify.org/iss-now.json', gotData, 'jsonp');
 }
 
 function gotData(data) {
@@ -12,11 +10,20 @@ function gotData(data) {
 
 function draw() {
   background(0);
-
   fill(255)
-  text("How many people are in space right now?", 100,100)
+  // text("How many people are in space right now?", 100,100)
+  // if (spaceData) {
+  //   text(spaceData.number, 150,150)
+  // }
+  // text("What are the names of the people in the spacecraft?", 100,200)
+  // if (spaceData){
+  //   text(spaceData.people[0].name, 150,250)
+  //   text(spaceData.people[1].name, 150,300)
+  //   text(spaceData.people[2].name, 150,350)
+  // }
+  text("What is the location of the International Space Station?", 100,300)
   if (spaceData) {
-    text(spaceData.number, 150,150)
+    text("the longitudinal location is: "+" "+spaceData.iss_position.longitude+" and ", 150,350)
+    text("the latitudinal location is: "+" "+spaceData.iss_position.latitude, 370,350)
   }
-
 }
